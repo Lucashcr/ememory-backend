2d4d9b11944fb964abb3dca3db229952770e151a
Lucas Rocha <lucash.rocha@hotmail.com>
2025-06-09 19:35:28 -0300

    Refactor urls and remove scripts from pyproject

--- src/reviews/urls.py
+++ src/reviews/urls.py
@@ -4,12 +4,10 @@ from rest_framework.routers import DefaultRouter
 from reviews.views import ReviewInfoViewSet, SubjectsViewSet
 
 
-urlpatterns = [
-    # path("", ReviewsAPIView.as_view()),
-]
-
-
 router = DefaultRouter()
 router.register("subjects", SubjectsViewSet, "subjects")
 router.register("", ReviewInfoViewSet, "reviews")
-urlpatterns += router.urls
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
